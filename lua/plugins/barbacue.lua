fad07cafaa605afdd969cdcf8367531357bc1398
Tom Payne <twpayne@gmail.com>
2024-06-29 17:25:21 +0000

    further customize

--- lua/plugins/barbacue.lua
+++
@@ -1,4 +0,0 @@
-return {
-  -- add dracula
-  { "utilyre/barbecue.nvim" },
-}
---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,65 @@
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = {
+      char = "▏",
+      use_treesitter = true,
+      show_trailing_blankline_indent = false,
+      show_first_indent_level = true,
+      show_current_context = true,
+      show_current_context_start = true,
+      char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+      },
+    },
+    config = function()
+      vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
+      vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
+      vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
+      vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
+      vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
+      vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+
+      require("ibl").setup({
+        indent = {
+          char = "▏",
+          highlight = {
+            "IndentBlanklineIndent1",
+            "IndentBlanklineIndent2",
+            "IndentBlanklineIndent3",
+            "IndentBlanklineIndent4",
+            "IndentBlanklineIndent5",
+            "IndentBlanklineIndent6",
+          },
+        },
+        scope = {
+          enabled = true,
+          show_start = true,
+          show_end = true,
+          include = {
+            node_type = {
+              ["*"] = {
+                "class",
+                "function",
+                "method",
+                "block",
+                "list_literal",
+                "selector",
+                "^if",
+                "^table",
+                "if_statement",
+                "while",
+                "for",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+}
